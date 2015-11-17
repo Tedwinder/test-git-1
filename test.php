6c7e92f7970b6ed185f349c5349b13a7e7059361
Ted Callow <TedCallow76@gmail.com>
2015-11-16 17:05:02 -0800

    updates test.php

---
+++ test.php
@@ -0,0 +1,23 @@
+<html>
+<head><</head>
+
+<body>
+
+
+<h1>Imagine this is a cool web page!</h1>
+<p>version 3</p>
+<p>Different Clever content goes here</p>
+<p>This is also clever content</p>
+<p>Clever content goes here</p>
+<p>Clever content goes here</p>
+<p>Clever content goes here</p>
+
+</body>
+
+
+
+
+
+
+
+</html>
